d5d8fb92a542d77a9d9d9a0f2f88538fa71a973e
Elvira Dzhumalieva <elvira.jumalieva@gmail.com>
2023-03-16 14:12:09 +0000

    Added ternary operator to solve problem if there isn't imageto certain news.

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Loader from "../components/Loader";
+import noPhoto from "../assets/no-photo.svg";
 
 function News() {
   const [news, setNews] = useState([]);
@@ -47,20 +48,33 @@ function News() {
               target="_blank"
               className="border border-gray-400 m-2 p-2 rounded hover:text-[rgb(234,179,8)]"
             >
-              <div>
-                <h2 className="font-bold text-[1.1em]">{item.name}</h2>
-                {/* <img src={item.image.thumbnail.contentUrl} alt="" /> */}
-                <div>
-                  {/* <img
-                      src={item.provider[0].image.thumbnail.contentUrl}
-                      alt=""
-                    /> */}
-                  <span className="text-sm  block my-5 italic">
-                    {item.provider[0].name}
-                  </span>
-                </div>
-                <p className="text-justify">{item.description}</p>
+              <div className="flex">
+                <h2 className="font-bold text-[1.1em] w-[80%]">{item.name}</h2>
+                <img
+                  src={
+                    item.image === undefined
+                      ? noPhoto
+                      : item.image.thumbnail.contentUrl
+                  }
+                  className="w-20 h-20"
+                  alt=""
+                />
               </div>
+              <div className="flex items-center space-x-2">
+                <img
+                  src={
+                    item.provider[0].image === undefined
+                      ? noPhoto
+                      : item.provider[0].image.thumbnail.contentUrl
+                  }
+                  alt=""
+                  className="w-5 h-5"
+                />
+                <span className="text-sm  block my-5 italic">
+                  {item.provider[0].name}
+                </span>
+              </div>
+              <p className="text-justify">{item.description}</p>
             </a>
           );
         })}
